5ba0351f795bad09ea5284c6caafaf01de37d9c9
Jessy <ernatusjessy@gmail.com>
2024-01-31 13:50:48 +0100

    update code

--- weather_station/weather_station.ino
+++ weather_station/weather_station.ino
@@ -1,20 +1,42 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <ArduinoJson.h>
 
-const char* ssid = "Votre_SSID";
-const char* password = "Votre_Mot_de_Passe";
-const char* serverName = "http://adresse_ip_du_serveur:3000/weather/";
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 
-const int potPin = 34; // Remplacer par le pin de votre potentiomètre
+#define SDA_PIN 21
+#define SCL_PIN 22
+
+const char* ssid        = "jessy";
+const char* password    = "hmuj3310";
+const char* serverName  = "http://192.168.77.219:3002/weather/";
+const int potPin        = 34;
+int potValue            = 0;
+JsonDocument docx;
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
   Serial.begin(115200);
+  Wire.begin(SDA_PIN, SCL_PIN);
+  delay(1000);
+
+  if(!display.begin(SSD1306_BLACK, 0x3C)) {
+    Serial.println(F("Erreur lors de l'initialisation de l'écran OLED"));
+    for(;;);
+  }
+
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connexion au WiFi...");
   }
   Serial.println("Connecté au WiFi");
+  display.clearDisplay();
 }
 
 String getCityFromPotValue(int value) {
@@ -28,22 +50,38 @@ String getCityFromPotValue(int value) {
 }
 
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
-    int potValue = analogRead(potPin); // Lire la valeur du potentiomètre
+    if (WiFi.status() == WL_CONNECTED) {
+    potValue = analogRead(potPin); // Lire la valeur du potentiomètre
     String city = getCityFromPotValue(potValue); // Obtenir le nom de la ville
-
+    
     HTTPClient http;
     http.begin(String(serverName) + city);
     int httpResponseCode = http.GET();
     
     if (httpResponseCode > 0) {
+
       String response = http.getString();
-      Serial.println(response); // Traitement de la réponse
-    } else {
+      deserializeJson(docx ,response);
+ // Traitement de la réponse
+    } else {  
       Serial.print("Erreur lors de la requête: ");
       Serial.println(httpResponseCode);
     }
     http.end();
   }
-  delay(60000); // Faire une requête toutes les 60 secondes
+
+  String cityName = docx[String("name")];
+  String countryName = docx[String("country")];
+  String placeTemperature = docx[String("temp_c")];
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(10, 10);
+
+  display.println("Ville",cityName);
+  display.println("Pays",countryName);
+  display.println("Temperature",placeTemperature);
+
+  delay(5000);
+  display.display();
+  display.clearDisplay();
 }
