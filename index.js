a132d0920924654fae1b5159eb3b24bb777fd5fd
Diego Da Silva M <78415421+diedasilva@users.noreply.github.com>
2024-01-31 10:57:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 3002
-const axios = require('axios'); // Axios pour faire des requêtes HTTP
-const apiKey = 'c4a6546ceda64b05ba7134102242901';
-const cities = ['Paris', 'London', 'Lisbon', 'Rome', 'Moscow', 'New York'];
+const express = require("express");
+const app = express();
+const port = 3002;
+const axios = require("axios"); // Axios pour faire des requêtes HTTP
+const apiKey = "c4a6546ceda64b05ba7134102242901";
+const cities = ["Paris", "London", "Lisbon", "Rome", "Moscow", "New York"];
 let weatherData = {};
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 // Fonction pour récupérer les données météo
 const fetchWeatherData = async () => {
   for (let city of cities) {
     const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
     try {
       const response = await axios.get(url);
-      weatherData[city] = response.data;
+      // Extraire uniquement le nom de la ville, le pays et la température en Celsius
+      const { name, country } = response.data.location;
+      const { temp_c } = response.data.current;
+
+      // Stocker ces informations dans weatherData
+      weatherData[city] = { name, country, temp_c: temp_c };
     } catch (error) {
-      console.error(`Erreur lors de la récupération des données pour ${city}:`, error);
+      console.error(
+        `Erreur lors de la récupération des données pour ${city}:`,
+        error
+      );
     }
   }
 };
@@ -29,13 +37,13 @@ const fetchWeatherData = async () => {
 fetchWeatherData(); // Première récupération au démarrage
 
 // Route pour obtenir les données météo d'une ville spécifique
-app.get('/weather/:city', (req, res) => {
+app.get("/weather/:city", (req, res) => {
   //fetchWeatherData();
   const city = req.params.city;
   const data = weatherData[city];
   if (data) {
     res.json(data);
   } else {
-    res.status(404).send('Données non trouvées pour la ville demandée');
+    res.status(404).send("Données non trouvées pour la ville demandée");
   }
 });
